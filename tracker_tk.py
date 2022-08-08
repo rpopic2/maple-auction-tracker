7cca2b4788f9dc75e496452f985d9f153317f487
rpopic2 <rpopic2@gmail.com>
2022-08-08 20:20:43 +0900

    added keyboard  shortcuts help

--- tracker_tk.py
+++ tracker_tk.py
@@ -51,6 +51,8 @@ def print_welcome_screen():
     uniques = df.drop_duplicates(subset='itemname', keep='last')
     outputLable.config(text=f"{getHelpText}\n{uniques}")
 
+getHelpText = "?를 입력하여 도움말 표시"
+
 root = Tk()
 root.title(f"메이플 옥션 트래커 v.{tracker_main.__version__}")
 frm = ttk.Frame(root, padding=10)
@@ -59,7 +61,6 @@ frm.grid()
 mainEntry = ttk.Entry(frm)
 mainEntry.grid(row=0, column=0)
 mainEntry.focus()
-
 
 ttk.Button(frm, text="Enter", command=onEnter).grid(row=0, column=1)
 root.bind("<Return>", dummyOnEnter)
@@ -70,7 +71,6 @@ root.bind("<Up>", loadLastCmd)
 statusbarLable = ttk.Label(frm)
 statusbarLable.grid(row=2, column=0)
 df = tracker_main.getData()
-getHelpText = "?를 입력하여 도움말 표시"
 outputLable = ttk.Label(frm)
 print_welcome_screen()
 
