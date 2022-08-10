4c844e50f741d98ea884eac9637ededbc9a47343
rpopic2 <rpopic2@gmail.com>
2022-08-10 16:19:25 +0900

    new alg for parseBigNums

--- koreanParser.py
+++ koreanParser.py
@@ -1,22 +1,33 @@
-#utils
+# utils
+
+from ast import parse
+
+
+unitdict = {
+    '십': 1,
+    '백': 2,
+    '천': 3,
+    '만': 4,
+    '억': 8,
+    '조': 12,
+}
+units = unitdict.keys()
+
+
 def parseNum(input: str) -> int:
     if ',' in input:
         input = input.replace(',', '')
-    if '만' in input:
-        return parseBigNums(input, '만', 4)
-    elif '억' in input:
-        return parseBigNums(input, '억', 8)
-    elif '조' in input:
-        return parseBigNums(input, '조', 12)
-    elif '.' in input:
-        return parseNum(input + '만')
-    else:
-        return int(input)
+    for c in input:
+        if c in unitdict:
+            return parseBigNums(input, c, unitdict[c])
+    return int(input)
 
-def parseBigNums(input, unit, e):
+
+def parseBigNums(input, unit, exponential):
     i = input.index(unit)
     n = float(input[:i])
-    return int(n * pow(10, e))
+    return int(n * pow(10, exponential))
+
 
 def parseCmd(input: str):
     return input.split(' ')
--- test.py
+++ test.py
@@ -6,12 +6,14 @@ from datetime import date
 class parsertest(unittest.TestCase):
     def test_parse_number(self):
         self.assertEqual(25000000, koreanParser.parseNum("2500만"))
-        self.assertEqual(25000, koreanParser.parseNum("2.5"))
         self.assertEqual(25000, koreanParser.parseNum("2.5만"))
         self.assertEqual(25000, koreanParser.parseNum("25000"))
         self.assertEqual(25000, koreanParser.parseNum("25,000"))
         self.assertEqual(250000000, koreanParser.parseNum("2.5억"))
-        
+        # self.assertEqual(250000000, koreanParser.parseNum("5천만"))
+
     def test_parse_command(self):
-        self.assertEqual(["아무개아이템", "2.5만"], koreanParser.parseCmd("아무개아이템 2.5만"))
-        self.assertEqual(["레인디어의창", "2.5만"], koreanParser.parseCmd("레인디어의창 2.5만"))
+        self.assertEqual(["아무개아이템", "2.5만"],
+                         koreanParser.parseCmd("아무개아이템 2.5만"))
+        self.assertEqual(["레인디어의창", "2.5만"],
+                         koreanParser.parseCmd("레인디어의창 2.5만"))
